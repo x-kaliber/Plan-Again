276698033099b1042e0435dc7df656be606092a3
x-kaliber <tobulidis@gmail.com>
2020-05-05 15:00:24 +0100

    Basic movement
    We have added the basic movement of the character.
    We have made a place holder for a tilemap.
    We have made a place holder for a character

---
+++ Plan Again/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D MyRigidBody;
+    private Vector3 Change;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MyRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Change = Vector3.zero;
+        Change.x = Input.GetAxisRaw("Horizontal");
+        Change.y = Input.GetAxisRaw("Vertical");
+        Debug.Log(Change);
+
+        if(Change != Vector3.zero)
+        {
+            MoveCharacter();
+        }
+    }
+
+    void MoveCharacter()
+    {
+        MyRigidBody.MovePosition(
+            transform.position + Change * speed * Time.deltaTime);
+    }
+}
