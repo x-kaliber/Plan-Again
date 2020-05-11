3d8c0d8920ca625d1dcf7a8a9d8118c43d85cc81
x-kaliber <tobulidis@gmail.com>
2020-05-11 18:22:26 +0100

    Enemy Move and Chase
    We made the animation for the enemy log.
    The log can chace us...
    and  it has a zone that it can chace  and a zone set for Attacking,

---
+++ Plan Again/Assets/Scripts/Log.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Log : Enemy
+{
+
+    public Transform target;
+    public Transform HomePosition;
+    public float ChaseRadius;
+    public float AttackRadius;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckDistance();
+    }
+
+    void CheckDistance()
+    {
+        if(Vector3.Distance(target.position, transform.position) <= ChaseRadius && Vector3.Distance(target.position, transform.position) > AttackRadius)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, MoveSpeed * Time.deltaTime);
+        }
+    }
+}
