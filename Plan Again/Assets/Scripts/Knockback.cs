2f52aeabb528ab7056ebaccc873461634dff1a38
x-kaliber <tobulidis@gmail.com>
2020-05-12 04:40:17 +0100

    Woked on enemy
    Worked on the enemy script and the physics and interaction of the enemy with the enviroment

---
+++ Plan Again/Assets/Scripts/Knockback.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class Knockback : MonoBehaviour
+{
+    public float thrust;
+    public float KnockTime;
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Rigidbody2D Enemy = other.GetComponent<Rigidbody2D>();
+            if(Enemy != null)
+            {
+                Vector2 difference = Enemy.transform.position - transform.position;
+                difference = difference.normalized * thrust;
+                Enemy.AddForce(difference, ForceMode2D.Impulse);
+                StartCoroutine(KnockCo(Enemy));
+            }
+        }
+    }
+
+    private IEnumerator KnockCo(Rigidbody2D Enemy)
+    {
+        if(Enemy != null)
+        {
+            yield return new WaitForSeconds(KnockTime);
+            Enemy.velocity = Vector2.zero;
+        }
+    }
+}
--- Plan Again/Assets/Scripts/Log.cs
+++ Plan Again/Assets/Scripts/Log.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Log : Enemy
 {
-
+    private Rigidbody2D myRigidbody;
     public Transform target;
     public Transform HomePosition;
     public float ChaseRadius;
@@ -14,6 +14,7 @@ public class Log : Enemy
     // Start is called before the first frame update
     void Start()
     {
+        myRigidbody = GetComponent<Rigidbody2D>();
         target = GameObject.FindWithTag("Player").transform;
     }
 
@@ -27,7 +28,8 @@ public class Log : Enemy
     {
         if(Vector3.Distance(target.position, transform.position) <= ChaseRadius && Vector3.Distance(target.position, transform.position) > AttackRadius)
         {
-            transform.position = Vector3.MoveTowards(transform.position, target.position, MoveSpeed * Time.deltaTime);
+            Vector3 temp = Vector3.MoveTowards(transform.position, target.position, MoveSpeed * Time.deltaTime);
+            myRigidbody.MovePosition(temp);
         }
     }
 }
