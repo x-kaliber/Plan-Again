3dcd4fb4357c6bb2b3b33650c8c39b92024cfee5
x-kaliber <tobulidis@gmail.com>
2020-05-14 22:09:12 +0100

    Interactable Chest
    We made an interactable chest
    We made an inventory ( placeholder)
    Made one item (key)
    organized the workArea.
    Preped for the creation of a functional inventory.
    Preped for creating more items.

---
+++ Plan Again/Assets/Scripts/Objects/TreasureChest.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TreasureChest : Interactable
+{
+    public Item Contents;
+    public Inventory playerInventory;
+    public bool isOpen;
+    public Signal raiseItem;
+    public GameObject dialogBox;
+    public Text dialogText;
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && PlayerInRange )
+        {
+            if (!isOpen)
+            {
+                //Open Chest
+                OpenChest();
+            }
+            else
+            {
+                //Chest is Open
+                ChestIsOpen();
+            }
+        }
+    }
+
+    public void OpenChest()
+    {
+        //Dialog Window on
+        dialogBox.SetActive(true);
+        //Dialog text = contents text
+        dialogText.text = Contents.itemDescription;
+        //Add contents to the inventory
+        playerInventory.AddItem(Contents);
+        playerInventory.currentItem = Contents;
+        //Rasie the signal to animate correctly
+        raiseItem.Raise();
+        //set the chest to opened
+        isOpen = true;
+        //raise the context clue
+        context.Raise();
+        //activate the animation
+        anim.SetBool("Opened", true);
+    }
+
+    public void ChestIsOpen()
+    {
+            //dialog off
+            dialogBox.SetActive(false);
+            //raise the signal to player to stop animate
+            raiseItem.Raise();
+
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !other.isTrigger && !isOpen)
+        {
+            context.Raise();
+            PlayerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !other.isTrigger && !isOpen)
+            context.Raise();
+        PlayerInRange = false;
+    }
+}
