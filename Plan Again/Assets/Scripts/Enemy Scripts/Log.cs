f5f11543c804da3550c509755b64e8a60e81bb46
x-kaliber <tobulidis@gmail.com>
2020-05-15 15:11:00 +0100

    Patrolling enemy set up of another scene
    We made a prefab for patroling enemies
    We made the script for the enemies.
    We made a new scene that is the underground  of the hidden door in the house.

---
+++ Plan Again/Assets/Scripts/Enemy Scripts/Log.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Log : Enemy
+{
+    public Rigidbody2D myRigidbody;
+    public Transform target;
+    public Transform HomePosition;
+    public float ChaseRadius;
+    public float AttackRadius;
+    public Animator anim;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentState = EnemyState.idle;
+        myRigidbody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        target = GameObject.FindWithTag("Player").transform;
+        anim.SetBool("WakeUp", true);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        CheckDistance();
+    }
+
+    public virtual void CheckDistance()
+    {
+        if(Vector3.Distance(target.position, transform.position) <= ChaseRadius && Vector3.Distance(target.position, transform.position) > AttackRadius)
+        {
+            if(currentState == EnemyState.idle || currentState == EnemyState.walk || currentState != EnemyState.stagger)
+            { 
+            Vector3 temp = Vector3.MoveTowards(transform.position, target.position, MoveSpeed * Time.deltaTime);
+            ChangeAnim(temp - transform.position);
+            myRigidbody.MovePosition(temp);
+            ChangeState(EnemyState.walk);
+            anim.SetBool("WakeUp", true);
+            }
+        }
+        else if(Vector3.Distance(target.position, transform.position) > ChaseRadius)
+        {
+            anim.SetBool("WakeUp", false);
+        }
+    }
+    private void SetAnimFloat(Vector2 setVector)
+    {
+        anim.SetFloat("MoveX", setVector.x);
+        anim.SetFloat("MoveY", setVector.y);
+    }
+    public void ChangeAnim(Vector2 direction)
+    {
+        if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
+        {
+            if(direction.x > 0)
+            {
+                SetAnimFloat(Vector2.right);
+            }
+            else if(direction.x < 0)
+            {
+                SetAnimFloat(Vector2.left);
+            }
+        }
+        else if(Mathf.Abs(direction.x) < Mathf.Abs(direction.y))
+        {
+            if(direction.y > 0)
+            {
+                SetAnimFloat(Vector2.up);
+            }
+            else if(direction.y < 0)
+            {
+                SetAnimFloat(Vector2.down);
+            }
+        }
+    }
+
+    private void ChangeState(EnemyState newState)
+    {
+        if ( currentState != newState)
+        {
+            currentState = newState;
+        }
+    }
+}
---
+++ Plan Again/Assets/Scripts/Enemy Scripts/PatrolLog.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolLog : Log
+{
+    public Transform[] path;
+    public int currentPoint;
+    public Transform currentGoal;
+    public float roundingDistance;
+
+    public override void CheckDistance()
+    {
+        if (Vector3.Distance(target.position, transform.position) <= ChaseRadius && Vector3.Distance(target.position, transform.position) > AttackRadius)
+        {
+            if (currentState == EnemyState.idle || currentState == EnemyState.walk || currentState != EnemyState.stagger)
+            {
+                Vector3 temp = Vector3.MoveTowards(transform.position, target.position, MoveSpeed * Time.deltaTime);
+                ChangeAnim(temp - transform.position);
+                myRigidbody.MovePosition(temp);
+                //ChangeState(EnemyState.walk);
+                anim.SetBool("WakeUp", true);
+            }
+        }
+        else if (Vector3.Distance(target.position, transform.position) > ChaseRadius)
+        {
+            if (Vector3.Distance(transform.position, path[currentPoint].position) > roundingDistance)
+            {
+                Vector3 temp = Vector3.MoveTowards(transform.position, path[currentPoint].position, MoveSpeed * Time.deltaTime);
+                ChangeAnim(temp - transform.position);
+                myRigidbody.MovePosition(temp);
+            }
+            else
+            {
+                ChangeGoal();
+            }
+        }
+    }
+
+    private void ChangeGoal()
+    {
+        if(currentPoint == path.Length - 1)
+        {
+            currentPoint = 0;
+            currentGoal = path[0];
+        }
+        else
+        {
+            currentPoint++;
+            currentGoal = path[currentPoint];
+        }
+    }
+}
