fabfa9a6a717a4b8a699afb41f72d2af035ec010
x-kaliber <tobulidis@gmail.com>
2020-05-18 06:34:19 +0100

    Added invisible Hunger meter

---
+++ Plan Again/Assets/Scripts/PlayerScripts/PlayerAttributes.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerAttributes : MonoBehaviour
+{
+    public static float hunger;
+    public  float MaxHunger;
+
+    void Update()
+    {
+        if( hunger < MaxHunger)
+        {
+
+            hunger += 0.5f * Time.deltaTime;
+        }
+
+        if(hunger >= MaxHunger)
+        {
+            Die();
+        }
+    }
+
+    void OntriggerEnter2d(Collider2D other)
+    {
+        if(other.tag == "Food")
+        {
+            hunger = hunger - 25;
+        }
+    }
+
+    void OntriggerExit2D(Collider2D other)
+    {
+
+    }
+
+    void Die()
+    {
+        this.gameObject.SetActive(false);
+        hunger = MaxHunger;
+        SceneManager.LoadScene("EndGame");
+    }
+}
